2d7b59e4b23040d59e29a62159bc6b6138c14bc8
wangsterj <wang.j@berkeley.edu>
2019-07-15 11:56:12 -0700

    Working on array case

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,5 +5,28 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
-  console.log('hi');
+  //base cases:
+    // number
+    // String
+    // boolean
+    // undefined null
+  // special cases
+    // arrays
+    
+  if (Array.isArray(obj)) {
+    var result = "";
+    var first = true;
+    for (var i =0; i < obj.length; i++) {
+      if (!first) {
+        result += ", " + stringifyJSON(obj[i]);
+        first = false;
+      }
+      else {
+        result += stringifyJSON(obj[i]);
+      }
+    }
+    return result;
+  }
+    // objects
+  return ''+obj;
 };

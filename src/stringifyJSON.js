f0f88f277e922650129b39284738d561e2467174
wangsterj <wang.j@berkeley.edu>
2019-07-15 15:16:32 -0700

    Passed stringifyJSON specs

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -12,21 +12,41 @@ var stringifyJSON = function(obj) {
     // undefined null
   // special cases
     // arrays
-    
+  if (typeof obj === "string") {
+    return '"' + obj + '"';
+  }
   if (Array.isArray(obj)) {
     var result = "";
     var first = true;
     for (var i =0; i < obj.length; i++) {
       if (!first) {
-        result += ", " + stringifyJSON(obj[i]);
-        first = false;
+        result += "," + stringifyJSON(obj[i]);
       }
       else {
         result += stringifyJSON(obj[i]);
+        first = false;
       }
     }
-    return result;
+    return "["+result+"]";
+  } else if (typeof obj === "object") {// objects
+      if (obj === null) {
+        return '' + obj;
+      }
+    var result = "";
+    var first = true;
+    for (var key in obj) {
+      if (obj[key] !== undefined && typeof obj[key] !== "function") {
+        if (!first) {
+          result += ',"'+key + '":' + stringifyJSON(obj[key]);
+        }
+        else {
+          result += '"'+key + '":' + stringifyJSON(obj[key]);
+          first = false;
+        }
+      }
+    }
+    return "{"+ result +"}";
   }
-    // objects
   return ''+obj;
 };
+
